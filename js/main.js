6a31656a810b165c9e273a2f6b053b02d5b82eb2
Gabriel PERINO <perinogabriel1@gmail.com>
2021-11-13 04:11:15 +0100

    ✨actication bouton suivant selon radio sur la page balancement image selon le genre choisi par l'utilisateur

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
-var suivant = document.getElementsById("btn-suivant");
-var interne = document.getElementById("interne");
-var externe = document.getElementById("externe");
-var feminin = document.getElementsById("feminin");
-var aide = document.getElementsById("aide");
-console.log(feminin);
+let suivant = document.getElementById("suivant");
+let interne = document.getElementById("interne");
+let externe = document.getElementById("externe");
 
-function testAide() {
-    if (aide.checked === true) { }
-}
+
 function suivantCondition() {
-    if (interne.checked == false && externe.checked == false) {
-        document.getElementsById("btn-suivant").disabled == true;
+    if (!(interne.checked) && !(externe.checked)) {
+        suivant.setAttribute("disabled", "disabled");
+    } else {
+        suivant.setAttribute("abled", "abled");
     }
 }
-function check() {
-    var ele = document.getElementsByName('interne/externe');
-    var flag = 0;
-    for (var i = 0; i < ele.length; i++) {
-        if (ele[i].checked == true) {
-            document.getElementById('btn-suivant').disabled = "";
-        }else{
-            suivant('btn-suivant').disabled = "disabled";
-        }
+suivant.addEventListener("click", suivantCondition());
+
+function changeImgGender() {
+    let masculin = document.getElementById("masculin");
+    let feminin = document.getElementById("feminin");
+    if (feminin.checked) {
+
+        // let titre = document.querySelectorAll('h2, .titre').style.color("#71BF45");
+        var thechoice = document.getElementById('feminin').value
+        document.getElementById("ident").src = "img/" + thechoice + ".png" ;
+
+
+    } else {
+        var thechoice = document.getElementById('masculin').value
+        // var titre = document.querySelectorAll('h2, .titre');
+        document.getElementById("ident").src = "img/" + thechoice + ".png" ;
     }
 }
+feminin.addEventListener("click", changeImgGender());
+masculin.addEventListener("click", changeImgGender());
+
+// #119EDA pour les hommes 
+// #71BF45 pour les femmes.
