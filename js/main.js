d5031f185546003a5949a6a865b4d2f63b037960
Gabriel PERINO <perinogabriel1@gmail.com>
2021-11-13 15:40:43 +0100

    ✨ condition button "suivant" to be enable

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 let suivant = document.getElementById("suivant");
 let interne = document.getElementById("interne");
 let externe = document.getElementById("externe");
-
+let transport = document.getElementById("transport");
+let aide1 = document.getElementById("aide1");
+let aide2 = document.getElementById("aide2");
+let aide3 = document.getElementById("aide3");
+let aide4 = document.getElementById("aide4");
 
 function suivantCondition() {
-    if (!(interne.checked) && !(externe.checked)) {
+    if (!(transport.checked) && !(aide1.checked) && !(aide2.checked) && !(aide3.checked) && !(aide4.checked)
+    ) {
         suivant.setAttribute("disabled", "disabled");
-    } else {
-        suivant.setAttribute("abled", "abled");
+    }
+    if ((transport.checked) || (aide1.checked) || (aide2.checked) || (aide3.checked) || (aide4.checked)) {
+        suivant.setAttribute("enabled", "enabled");
     }
 }
 suivant.addEventListener("click", suivantCondition());
@@ -19,13 +25,12 @@ function changeImgGender() {
 
         // let titre = document.querySelectorAll('h2, .titre').style.color("#71BF45");
         var thechoice = document.getElementById('feminin').value
-        document.getElementById("ident").src = "img/" + thechoice + ".png" ;
-
+        document.getElementById("ident").src = "img/" + thechoice + ".png";
 
     } else {
         var thechoice = document.getElementById('masculin').value
         // var titre = document.querySelectorAll('h2, .titre');
-        document.getElementById("ident").src = "img/" + thechoice + ".png" ;
+        document.getElementById("ident").src = "img/" + thechoice + ".png";
     }
 }
 feminin.addEventListener("click", changeImgGender());
